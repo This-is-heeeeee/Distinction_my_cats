839eff8224f0a8a94810e6d75709d4081a597bfb
This-is-heeeeee <lkh951212@gmail.com>
2021-06-02 20:19:11 +0900

    Detect cats

--- Detection_face.py
+++ Detection_face.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy
+import os
 
 cascade_filename = 'haarcascade_frontalcatface.xml'
 cascade_filename_ex = 'haarcascade_frontalcatface_extended.xml'
@@ -7,46 +8,112 @@ cascade_filename_ex = 'haarcascade_frontalcatface_extended.xml'
 SF = 1.05 #scale factor -> 1.05, 1.3 ...
 MN = 3 #minimum neighbours -> 3,4,5,6 ...
 
-def videoDetector(cam, cascade, cascade_ex):
-    while True:
+origin_path = "data"
+destination_path = "dataset"
+folders = ["img", "cam"]
 
-        ret, img = cam.read()
-        img = cv2.resize(img, dsize=None, fx=0.5, fy=0.5)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        results = cascade.detectMultiScale(gray, scaleFactor=SF, minNeighbors=MN)
-        results_ex = cascade_ex.detectMultiScale(gray, scaleFactor=SF, minNeighbors=MN)
+def videoDetector(cascade, cascade_ex):
+    fname = ''
 
-        for box in results:
-            x, y, w, h = box
-            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), thickness=2)
-        for box in results_ex :
-            x, y, w, h = box
-            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
+    for root, dirs, files in os.walk("{}/{}".format(origin_path, folders[1])):
+        #print(files)
+        for cam_file in files:
+            fname = cam_file
+            cam = cv2.VideoCapture("{}/{}".format(root, cam_file))
+            count = 0
+            count_ex = 0
+            while True:
+                ret, img = cam.read()
+                img = cv2.resize(img, dsize=None, fx=0.5, fy=0.5)
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                results = cascade.detectMultiScale(gray, scaleFactor=SF, minNeighbors=MN)
+                print(results)
+                print(len(results))
+                results_ex = cascade_ex.detectMultiScale(gray, scaleFactor=SF, minNeighbors=MN)
 
-        cv2.imshow('catface',img)
-        cv2.imwrite('result_video.mp4',img)
+                if count < 10:
+                    for i in range(len(results)):
+                        x, y, w, h = results[i]
+                        crop = img[y:y + h, x:x + w].copy()
+                        cv2.imwrite("{}/{}/{}_{}-{}.jpg".format(destination_path, folders[1], cam_file[0:-4], count, i), crop)
+                        if i == len(results)-1:
+                            count+=1
 
-        if cv2.waitKey(1) > 0:
-            break
+
+                if count_ex < 10:
+                    for i in range(len(results_ex)):
+                        x, y, w, h = results_ex[i]
+                        crop = img[y:y + h, x:x + w].copy()
+                        cv2.imwrite("{}/{}/{}_{}-{}.jpg".format(destination_path, folders[1], cam_file[0:-4], count_ex, i), crop)
+                        if i == len(results_ex)-1:
+                            count_ex+=1
+
+                """
+                if len(results) > 0 and count < 10:
+                    count = count + 1
+                    for box in results:
+                        x, y, w, h = box
+                        crop = img[y:y+h, x:x+w].copy()
+                        cv2.imwrite("{}/{}/{}_{}.jpg".format(destination_path, folders[1], cam_file[0:-4], count), crop)
+                        #cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), thickness=2)
+                if len(results_ex) > 0 and count_ex < 10:
+                    count_ex = count_ex + 1
+                    for box in results_ex:
+                        x, y, w, h = box
+                        crop = img[y:y+h, x:x+w].copy()
+                        cv2.imwrite("{}/{}/ex_{}_{}.jpg".format(destination_path, folders[1], cam_file[0:-4], count_ex), crop)
+                        #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+                """
+
+                cv2.imshow('catface', img)
+
+                if count >= 10 and count_ex >= 10 :
+                    break
+
+                if cv2.waitKey(1) > 0:
+                    break
+
+def imgDetector(cascade,cascade_ex):
+    for root, dirs, files in os.walk("{}/{}".format(origin_path, folders[0])):
+        for img_file in files:
+            img = cv2.imread("{}/{}".format(root,img_file))
+
+            img = cv2.resize(img, dsize=None, fx=0.1, fy=0.1)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            results = cascade.detectMultiScale(gray, scaleFactor=SF, minNeighbors=MN)
+            results_ex = cascade_ex.detectMultiScale(gray, scaleFactor=SF, minNeighbors=MN)
+
+            for i in range(len(results)):
+                x, y, w, h = results[i]
+                crop = img[y:y + h, x:x + w].copy()
+                cv2.imwrite("{}/{}/{}_{}.jpg".format(destination_path, folders[0], img_file[0:-4], i), crop)
+
+            for i in range(len(results_ex)):
+                x, y, w, h = results_ex[i]
+                crop = img[y:y + h, x:x + w].copy()
+                cv2.imwrite("{}/{}/ex_{}_{}.jpg".format(destination_path, folders[0], img_file[0:-4], i), crop)
+
+            """
+            if len(results) > 0:
+                for box in results:
+                    x, y, w, h = box
+                    crop = img[y:y + h, x:x + w].copy()
+                    cv2.imwrite("{}/{}/{}".format(destination_path, folders[0], img_file), crop)
+                    #cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), thickness=2)
+
+            if len(results_ex) > 0:
+                for box in results_ex:
+                    x, y, w, h = box
+                    crop = img[y:y + h, x:x + w].copy()
+                    cv2.imwrite("{}/{}/ex_{}".format(destination_path, folders[0], img_file), crop)
+                    #cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+            """
+
+            cv2.imshow('catface', img)
+            #cv2.imwrite("{}/{}/{}".format(destination_path, folders[0], img_file), img)
+            cv2.waitKey(5000)
 
 
-
-def imgDetector(img, cascade,cascade_ex):
-    img = cv2.resize(img, dsize=None, fx=0.1, fy=0.1)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    results = cascade.detectMultiScale(gray, scaleFactor=SF, minNeighbors=MN)
-    results_ex = cascade_ex.detectMultiScale(gray, scaleFactor=SF, minNeighbors=MN)
-
-    for box in results:
-        x, y, w, h = box
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), thickness=2)
-    for box in results_ex:
-        x, y, w, h = box
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
-
-    cv2.imshow('catface',img)
-    cv2.imwrite('result_image2.jpg', img)
-    cv2.waitKey(10000)
 
 
 
@@ -54,11 +121,15 @@ def main():
     cascade = cv2.CascadeClassifier(cascade_filename)
     cascade_ex = cv2.CascadeClassifier(cascade_filename_ex)
 
-    #cam = cv2.VideoCapture('autumn_video.mp4')
-    img = cv2.imread('autumn.jpg')
+    if not os.path.exists(destination_path):
+        os.mkdir(destination_path)
 
-    #videoDetector(cam, cascade, cascade_ex)
-    imgDetector(img, cascade, cascade_ex)
+    for folder in folders:
+        if not os.path.exists("{}/{}".format(destination_path, folder)):
+            os.mkdir("{}/{}".format(destination_path, folder))
+
+    videoDetector(cascade, cascade_ex)
+    imgDetector(cascade, cascade_ex)
 
 if __name__ == '__main__':
     main()
